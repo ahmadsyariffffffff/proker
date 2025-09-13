9911911672edd89bba2c9a3b6edb58a114047653
ahmad-uhm <kepodong304@gmail.com>
2025-09-14 00:27:22 +0800

    update footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -4,12 +4,12 @@ import { Link } from "react-router";
 export default function Footer() {
   return (
     <footer className="bg-green-700 text-white mt-8">
-      <div className="max-w-6xl mx-auto px-3 py-3 grid grid-cols-1 md:grid-cols-3 gap-8">
+      <div className="max-w-6xl mx-auto px-3 py-3 center grid-cols-1 md:grid-cols-3 gap-8">
         {/* Profil singkat */}
         <div>
-          <h2 className="text-lg font-bold mb-3">Desa Makmur</h2>
+          <h2 className="text-lg font-bold mb-3">Desa Panaikang</h2>
           <p className="text-sm">
-            Desa Makmur adalah desa yang terus berkembang dengan potensi wisata,
+            Desa panaikang adalah desa yang terus berkembang dengan potensi wisata,
             pertanian, dan UMKM lokal yang maju.
           </p>
         </div>
@@ -17,14 +17,14 @@ export default function Footer() {
         {/* Kontak singkat */}
         <div>
           <h2 className="text-lg font-bold mb-3">Kontak</h2>
-          <p className="text-sm">Alamat: Jl. Desa Makmur No. 1</p>
+          <p className="text-sm">Alamat: Jl. Desa Panaikang</p>
           <p className="text-sm">Telepon: 0812-3456-7890</p>
-          <p className="text-sm">Email: desamakmur@email.com</p>
+          <p className="text-sm">Email: desapanaikang@email.com</p>
         </div>
       </div>
 
       <div className="bg-green-800 text-center text-sm py-4">
-        © {new Date().getFullYear()} Desa Panaikang KKN UHM . Posko 5.
+        © {new Date().getFullYear()} KKN UHM Desa Panaikang. Posko 5.
       </div>
     </footer>
   );
