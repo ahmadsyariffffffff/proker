357a359143152cb4354aad8f751bb39054bd9a05
ahmad-uhm <kepodong304@gmail.com>
2025-09-22 03:10:16 +0800

    update home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,11 +4,11 @@ import { Link } from "react-router";
 export default function Home() {
   return (
     <div
-      className="p-6 min-h-screen bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: "url('/desa.jpg')" }} // gambar dari public/bg-desa.jpg
+      className="min-h-screen w-full bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: "url('/desa.jpg')" }} // ambil dari public/desa.jpg
     >
       {/* Overlay biar teks tetap terbaca */}
-      <div className="bg-black/40 min-h-screen p-6 rounded-xl">
+      <div className="bg-black/40 min-h-screen w-full px-6 py-12">
         {/* Sambutan Kepala Desa */}
         <section className="mb-8 text-center text-white">
           <h1 className="text-3xl font-bold mb-4">
@@ -34,26 +34,27 @@ export default function Home() {
 
         {/* Banner / Foto Desa */}
         <section>
-          <h2 className="text-2xl font-semibold mb-2 text-white">Kegiatan & Dokumentasi</h2>
+          <h2 className="text-2xl font-semibold mb-2 text-white">
+            Kegiatan & Dokumentasi
+          </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             <img
-  src="/desa.jpg"
-  alt="Banner Desa"
-  className="rounded-xl shadow-md transform transition duration-300 hover:scale-105"
-/>
+              src="/desa.jpg"
+              alt="Banner Desa"
+              className="rounded-xl shadow-md transform transition duration-300 hover:scale-105"
+            />
 
-<img
-  src="https://placehold.co/400x250?text=Kegiatan+1"
-  alt="Kegiatan Desa"
-  className="rounded-xl shadow-md transform transition duration-300 hover:scale-105"
-/>
+            <img
+              src="https://placehold.co/400x250?text=Kegiatan+1"
+              alt="Kegiatan Desa"
+              className="rounded-xl shadow-md transform transition duration-300 hover:scale-105"
+            />
 
-<img
-  src="https://placehold.co/400x250?text=Kegiatan+2"
-  alt="Kegiatan Desa"
-  className="rounded-xl shadow-md transform transition duration-300 hover:scale-105"
-/>
-
+            <img
+              src="https://placehold.co/400x250?text=Kegiatan+2"
+              alt="Kegiatan Desa"
+              className="rounded-xl shadow-md transform transition duration-300 hover:scale-105"
+            />
           </div>
         </section>
       </div>
