5f7447d66b644401bd9e8f925f45690792effcd9
ahmad-uhm <kepodong304@gmail.com>
2025-09-25 16:03:38 +0800

    update halaman

--- app/routes/halaman.tsx
+++ app/routes/halaman.tsx
@@ -24,7 +24,7 @@ export default function Kontak() {
         <div className="bg-white shadow rounded-xl overflow-hidden">
           <iframe
             title="Lokasi Desa"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3168.736682878898!2d-122.08424968469224!3d37.42199977982588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb5d55a4e8c0b%3A0xe4d4a1a9e3f2bbd6!2sGoogleplex!5e0!3m2!1sid!2sid!4v1693205023456!5m2!1sid!2sid"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18873.894949623074!2d119.57238220905724!3d-5.191476516576447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbee5eafa291fc3%3A0xe7f6e54298df8ea8!2sPanaikang%2C%20Kec.%20Pattallassang%2C%20Kabupaten%20Gowa%2C%20Sulawesi%20Selatan!5e1!3m2!1sid!2sid!4v1756401466949!5m2!1sid!2sid"
             width="100%"
             height="350"
             style={{ border: 0 }}
